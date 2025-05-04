81a6f1af130c2d64f0f0d6d1b1becb0901ce6965
Fazeel <fazeel0108@gmail.com>
2025-05-04 13:57:26 +0530

    Connected form.html with app.py

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return "Welcome to My Python Website! This is displayed from Python."
+    message = ""
+    if request.method == "POST":
+        name = request.form.get("name")
+        role = request.form.get("role").lower().strip()
+        age = request.form.get("age").strip()
+
+        if not age.isdigit():
+            message = "Please enter a valid age."
+        else:
+            age = int(age)
+            if role == "student":
+                message = f"Hello {name}, your age is {age}, you got {age}% discount."
+            elif role == "teacher":
+                message = f"Hello {name}, you can have it for free."
+            else:
+                message = "Role not recognized."
+
+    return render_template("form.htm", message=message)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
+
 
