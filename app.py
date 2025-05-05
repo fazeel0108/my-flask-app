012070f297d7a8cf57c5a7499ac22c0fe10bcd8a
Fazeel <fazeel0108@gmail.com>
2025-05-05 17:33:31 +0530

    Fix .gitignore and add Flask project

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def welcome():
-    return render_template('index.html')
+    return render_template('welcome.html')
 
 @app.route('/form', methods=['GET', 'POST'])
 def form():
     if request.method == 'POST':
         name = request.form['name']
-        role = request.form['Gamer']
+        role = request.form['role']
         age = request.form['age']
         return render_template('3.html', name=name, role=role, age=age)
-    return render_template('form.html')  # Serving the form initially
+    return render_template ('form.html')  # Show the form on GET
 
 if __name__ == '__main__':
     app.run(debug=True)
