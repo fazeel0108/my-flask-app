2435caee5be0d228743b7a8790b6033f273faa10
Fazeel <fazeel0108@gmail.com>
2025-05-05 11:50:02 +0530

    Added welcome landing page and connected to form

--- app.py
+++ app.py
@@ -2,25 +2,27 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
-def home():
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/form", methods=["GET", "POST"])
+def form():
     if request.method == "POST":
-        name = request.form.get("name")
-        role = request.form.get("role", "").strip().lower()
-        age = request.form.get("age", "").strip()
+        name = request.form["name"]
+        role = request.form["role"].strip().lower()
+        age = request.form["age"].strip()
 
         if not age.isdigit():
             return f"Hello {name}, please enter a valid age."
 
         age = int(age)
-
         if role == "student":
             return f"Hello {name}, your age is {age}, you got {age}% discount."
         elif role == "teacher":
             return f"Hello {name}, you can have it for free."
         else:
             return "Role not recognized."
-
     return render_template("form.html")
 
 if __name__ == "__main__":
